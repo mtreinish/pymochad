6b4b62ff5378f7f414b99daeada265aa7a3b4d48
Matthew Treinish <mtreinish@kortar.org>
2016-10-01 12:48:51 -0400

    Update gpl header with pymochad

--- pymochad/controller.py
+++ pymochad/controller.py
@@ -1,19 +1,19 @@
 # Copyright 2016 Matthew Treinish
 #
-# This file is part of pycmus
+# This file is part of pymochad
 #
-# pycmus is free software: you can redistribute it and/or modify
+# pymochad is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# pycmus is distributed in the hope that it will be useful,
+# pymochad is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with pycmus.  If not, see <http://www.gnu.org/licenses/>.
+# along with pymochad. If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 import socket
